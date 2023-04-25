555318f0d58c25f0a10ab3f46ea5e78b63dfc489
zhacheny <chenyi.zha@etu.utc.fr>
2023-04-25 14:00:12 +0200

    modified:   algo_file_attente.txt 	new file:   main.go 	new file:   model/controller/MutexController.go 	new file:   model/message/MutexMessage.go

---
+++ model/controller/MutexController.go
@@ -0,0 +1,87 @@
+package controller
+
+import (
+	"math"
+	"time"
+
+	mm "github.com/Szzx123/depot_reparti/model/MutexMessage"
+)
+
+type MutexController struct {
+	num     int               //number of site
+	tab     []mm.MutexMessage //register the latest statues of all sites
+	horloge int               //horloge local
+	channel chan string       //channel to communicate with basic application
+}
+
+func New_MutexController() *MutexController {
+	channel := make(chan string)
+	tab := make([]mm.MutexMessage, 5) //怎么样知道初始有多少site？
+	return &MutexController{
+		num:     1, //根据全局变量来？
+		horloge: 0,
+		tab:     tab,
+		channel: channel,
+	}
+}
+
+// Réception d’une demande de section critique ou de fin de section critique de l’application de base
+func (mc *MutexController) Receive_RequestSC() {
+	for {
+		select {
+		case instruction := <-mc.channel:
+			if instruction == "demandeSC" {
+				mc.horloge += 1
+				msg := mm.New_MutexMessage(mc.horloge, 1)
+				mc.tab[mc.num] = *msg
+				// envoyer( [requête] hi ) à tous les autres sites
+			} else if instruction == "finSC" {
+				mc.horloge += 1
+				msg := mm.New_MutexMessage(mc.horloge, 0)
+				mc.tab[mc.num] = *msg
+				// envoyer( [libération] hi ) à tous les autres sites.
+			}
+		default:
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+}
+
+// Réception d’un message de type requête
+// Réception d’un message de type libération
+// Réception d’un message de type accusé
+func (mc *MutexController) ExtMessage_Handler() {
+	for {
+		// recevoir( [type_message ] h ) de Sj
+		if type_message == "requête" {
+			mc.horloge = math.MaxInt(mc.horloge, ext_horloge) + 1
+			mc.tab[ext_num] = *mm.New_MutexMessage(mc.horloge, 1)
+			// envoyer( [accusé] hi ) à Sj
+			mc.Send_StartSC()
+		} else if type_message == "libération" {
+			mc.horloge = math.MaxInt(mc.horloge, ext_horloge) + 1
+			mc.tab[ext_num] = *mm.New_MutexMessage(mc.horloge, 0)
+			mc.Send_StartSC()
+		} else if type_message == "accusé" {
+			mc.horloge = math.MaxInt(mc.horloge, ext_horloge) + 1
+			if mc.tab[ext_num].typeMessage != 1 {
+				mc.tab[ext_num] = *mm.New_MutexMessage(mc.horloge, 2)
+			}
+			mc.Send_StartSC()
+		}
+	}	
+}
+
+//L’arrivée du message pourrait permettre de satisfaire une éventuelle demande de Si. 
+func (mc *MutexController) Send_StartSC() {
+	if mc.tab[mc.num] == 1 {
+		for k := range mc.tab {
+			if k != mc.num && (Tabi[i].date, i) >2 (Tabi[k].date, k) {
+				break
+			}
+		}
+		mc.channel <- "debutSC" // envoyer( [débutSC] ) à l’application de base 
+	}
+}
+
+
---
+++ model/message/MutexMessage.go
@@ -0,0 +1,43 @@
+package MutexMessage
+
+type MutexMessage struct {
+	horloge     int
+	typeMessage TypeMessage
+}
+
+type TypeMessage int
+
+const (
+	Release   TypeMessage = 0
+	Request   TypeMessage = 1
+	ACK       TypeMessage = 3
+	demandeSC TypeMessage = 4
+	startSC   TypeMessage = 5
+	endSC     TypeMessage = 6
+)
+
+func (tm TypeMessage) String() string {
+	switch tm {
+	case Release:
+		return "libération"
+	case Request:
+		return "requête"
+	case ACK:
+		return "accusé"
+	case demandeSC:
+		return "demandeSC"
+	case startSC:
+		return "débutSC"
+	case endSC:
+		return "finSC"
+	default:
+		return "unknown"
+	}
+}
+
+func New_MutexMessage(h int, typ TypeMessage) *MutexMessage {
+	return &MutexMessage{
+		horloge:     h,
+		typeMessage: typ,
+	}
+}
