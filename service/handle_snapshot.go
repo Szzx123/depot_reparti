ec252ed82c5eeb9e70224585f6f694b2512a65f3
Szzx123 <shizzxszzx@126.com>
2023-04-30 21:29:27 +0200

    添加snapshot按钮

--- service/handle_snapshot.go
+++ service/handle_snapshot.go
@@ -3,7 +3,6 @@ package service
 import (
 	"encoding/json"
 	"github.com/Szzx123/depot_reparti/model/message"
-	"github.com/Szzx123/depot_reparti/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"log"
@@ -46,7 +45,7 @@ func Snapshot_Handler(c *gin.Context) {
 			continue
 		}
 
-		utils.Msg_send(utils.Msg_format("receiver", "C"+msg.Site[1:]) + utils.Msg_format("type", "demandeSC") + utils.Msg_format("cargo", msg.Cargo) + utils.Msg_format("operation", msg.Type) + utils.Msg_format("quantity", msg.Quantity))
+		//utils.Msg_send(utils.Msg_format("receiver", "C"+msg.Site[1:]) + utils.Msg_format("type", "demandeSC") + utils.Msg_format("cargo", msg.Cargo) + utils.Msg_format("operation", msg.Type) + utils.Msg_format("quantity", msg.Quantity))
 		// 发送消息
 		if err := conn.WriteMessage(websocket.TextMessage, []byte("消息已收到")); err != nil {
 			log.Printf("发送消息失败: %s", err)
