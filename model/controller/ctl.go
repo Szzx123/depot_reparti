bef247dedfc7de200e6700a79f0b37a987e6a7b9
Szzx123 <shizzxszzx@126.com>
2023-05-01 18:49:39 +0200

    修改向量时钟

--- model/controller/ctl.go
+++ model/controller/ctl.go
@@ -230,7 +230,7 @@ func (ctl *Controller) Message_Handler(msg *message.MutexMessage) {
 		// envoyer( [accusé] hi ) à Sj
 		utils.Msg_send(utils.Msg_format("receiver", ext_num) + utils.Msg_format("type", "ack") +
 			utils.Msg_format("sender", ctl.num) + utils.Msg_format("horloge", strconv.Itoa(ctl.horloge)) +
-			utils.Msg_format("H1", strconv.Itoa(ctl.horloge_vec[0])) + utils.Msg_format("H1", strconv.Itoa(ctl.horloge_vec[1])) + utils.Msg_format("H1", strconv.Itoa(ctl.horloge_vec[2])))
+			utils.Msg_format("H1", strconv.Itoa(ctl.horloge_vec[0])) + utils.Msg_format("H2", strconv.Itoa(ctl.horloge_vec[1])) + utils.Msg_format("H3", strconv.Itoa(ctl.horloge_vec[2])))
 		ctl.Send_StartSC()
 		l.Println(ctl.num, ": ", ctl.tab) // test
 	case "release":
@@ -250,7 +250,7 @@ func (ctl *Controller) Message_Handler(msg *message.MutexMessage) {
 		stock_C := msg.Stock_C
 		ctl.tab[ext_num] = *msg
 		utils.Msg_send(utils.Msg_format("receiver", "A"+ctl.num[1:]) + utils.Msg_format("type", "updateSC") + utils.Msg_format("sender", ctl.num) + utils.Msg_format("horloge", strconv.Itoa(ctl.horloge)) + utils.Msg_format("A", strconv.Itoa(stock_A)) + utils.Msg_format("B", strconv.Itoa(stock_B)) + utils.Msg_format("C", strconv.Itoa(stock_C)) +
-			utils.Msg_format("H1", strconv.Itoa(ctl.horloge_vec[0])) + utils.Msg_format("H1", strconv.Itoa(ctl.horloge_vec[1])) + utils.Msg_format("H1", strconv.Itoa(ctl.horloge_vec[2])))
+			utils.Msg_format("H1", strconv.Itoa(ctl.horloge_vec[0])) + utils.Msg_format("H2", strconv.Itoa(ctl.horloge_vec[1])) + utils.Msg_format("H3", strconv.Itoa(ctl.horloge_vec[2])))
 		ctl.Send_StartSC()
 		l.Println(ctl.num, ": ", ctl.tab) // test
 	case "ack":
