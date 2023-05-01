3e35013ff89c919c2900cf462faf937e543a3f1e
jieniyu <jieni.yu42@gmail.com>
2023-05-01 23:07:06 +0200

    add update cargo websocket

--- model/message/CargoOperation.go
+++ model/message/CargoOperation.go
@@ -1,8 +1,12 @@
 package message
 
-type Message struct {
-	Site     string `json:"site"`  //num of site
-	Cargo    string `json:"cargo"` // cargo name
-	Type     string `json:"type"`  // in or out
-	Quantity string `json:"quantity"`
+type CargoMessage struct {
+	Site        string `json:"site"` //num of site
+	TypeMessage string `json:"type_message"`
+	Cargo       string `json:"cargo"` // cargo name
+	Type        string `json:"type"`  // in or out
+	Quantity    string `json:"quantity"`
+	Stock_A     int    `json:"stock_A"`
+	Stock_B     int    `json:"stock_B"`
+	Stock_C     int    `json:"stock_C"`
 }
--- service/handle_cargo.go
+++ service/handle_cargo.go
@@ -2,11 +2,11 @@ package service
 
 import (
 	"encoding/json"
+	"github.com/Szzx123/depot_reparti/utils"
 	"log"
 	"net/http"
 
 	"github.com/Szzx123/depot_reparti/model/message"
-	"github.com/Szzx123/depot_reparti/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
@@ -47,22 +47,24 @@ func Cargo_Receive_Handler(c *gin.Context) {
 		copy(message_content, message_json)
 
 		// 解析 JSON
-		var msg message.Message
+		var msg message.CargoMessage
 		if err := json.Unmarshal(message_content, &msg); err != nil {
 			log.Printf("解析 JSON 失败: %s", err)
 			continue
 		}
 
-		if len(msg.Cargo) == 0 || len(msg.Type) == 0 || len(msg.Quantity) == 0 {
-			log.Printf("Blank Message")
-			continue
-		}
+		if msg.TypeMessage == "operateCargo" {
+			if len(msg.Cargo) == 0 || len(msg.Type) == 0 || len(msg.Quantity) == 0 {
+				log.Printf("Blank Message")
+				continue
+			}
 
-		utils.Msg_send(utils.Msg_format("receiver", "C"+msg.Site[1:]) + utils.Msg_format("type", "demandeSC") + utils.Msg_format("cargo", msg.Cargo) + utils.Msg_format("operation", msg.Type) + utils.Msg_format("quantity", msg.Quantity))
-		// 发送消息
-		if err := ConnCargo.WriteMessage(websocket.TextMessage, []byte("消息已收到")); err != nil {
-			log.Printf("发送消息失败: %s", err)
-			break
+			utils.Msg_send(utils.Msg_format("receiver", "C"+msg.Site[1:]) + utils.Msg_format("type", "demandeSC") + utils.Msg_format("cargo", msg.Cargo) + utils.Msg_format("operation", msg.Type) + utils.Msg_format("quantity", msg.Quantity))
+			// 发送消息
+			if err := ConnCargo.WriteMessage(websocket.TextMessage, []byte("消息已收到")); err != nil {
+				log.Printf("发送消息失败: %s", err)
+				break
+			}
 		}
 	}
 }
