05184430a6d0da05ccf06972f0ddb02faf636428
Szzx123 <shizzxszzx@126.com>
2023-05-01 17:59:18 +0200

    抽象websocket

--- service/handle_snapshot.go
+++ service/handle_snapshot.go
@@ -25,40 +25,40 @@ func Snapshot_Handler(c *gin.Context) {
 		return
 	}
 	defer Conn.Close()
-	for {
-		// 读取消息
-		//_, message_json, err := conn.ReadMessage()
-		_, message_json, err := Conn.ReadMessage()
-		if err != nil {
-			log.Printf("读取消息失败: %s", err)
-			break
-		}
-
-		// 暂存消息内容
-		message_content := make([]byte, len(message_json))
-		copy(message_content, message_json)
-
-		// 解析 JSON
-		var msg message.SnapshotMessage
-		if err := json.Unmarshal(message_content, &msg); err != nil {
-			log.Printf("解析 JSON 失败: %s", err)
-			continue
-		}
-
-		if msg.TypeMessage == "generateSnapshot" {
-			// 发送消息
-			err = Conn.WriteMessage(websocket.TextMessage, message_json)
-			if err != nil {
-				log.Fatal(err)
-			}
-		} else { // demande Snapshot
-			utils.Msg_send(utils.Msg_format("receiver", "C"+msg.Site[1:]) + utils.Msg_format("type", "demandeSnapshot"))
-			// 发送消息
-			if err := Conn.WriteMessage(websocket.TextMessage, []byte("demandeSnapshot已收到")); err != nil {
-				log.Printf("发送消息失败: %s", err)
-				break
-			}
-		}
-
+	//for {
+	// 读取消息
+	//_, message_json, err := conn.ReadMessage()
+	_, message_json, err := Conn.ReadMessage()
+	if err != nil {
+		log.Printf("读取消息失败: %s", err)
+		//break
 	}
+
+	// 暂存消息内容
+	message_content := make([]byte, len(message_json))
+	copy(message_content, message_json)
+
+	// 解析 JSON
+	var msg message.SnapshotMessage
+	if err := json.Unmarshal(message_content, &msg); err != nil {
+		log.Printf("解析 JSON 失败: %s", err)
+		//continue
+	}
+
+	if msg.TypeMessage == "generateSnapshot" {
+		// 发送消息
+		err = Conn.WriteMessage(websocket.TextMessage, message_json)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else { // demande Snapshot
+		utils.Msg_send(utils.Msg_format("receiver", "C"+msg.Site[1:]) + utils.Msg_format("type", "demandeSnapshot"))
+		// 发送消息
+		if err := Conn.WriteMessage(websocket.TextMessage, []byte("demandeSnapshot已收到")); err != nil {
+			log.Printf("发送消息失败: %s", err)
+			//break
+		}
+	}
+
+	//}
 }
